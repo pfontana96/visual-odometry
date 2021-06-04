06dc19c08045aae22c05fcd84dcf1f32c5529fbc
Pedro Fontana <pedro.fonta.a.1996@gmail.com>
2021-06-04 19:19:44 -0300

    Adds cv_bridge support for Python3 and cv_bridge

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # ottobot
 ROS based autonomous mobile robot
+
+## catkin_workspace and cv_bridge support with Python3 and OpenCV 4.1.1 (Jetson Nano)
+
+Create Catkin workspace
+
+```shell
+mkdir -p ~/catkin_ws/src -DPYTHON_EXECUTABLE=/usr/bin/python3
+cd ~/catkin_ws
+```
+
+Download `cv_bridge` package for `noetic`. Although the Nano comes with Ubuntu 18.04(i.e. `melodic`), `noetic` is the one with support for installed OpenCv 4 (version installed by default on the Nano).
+
+```shell
+cd src
+git clone -b noetic https://github.com/ros-perception/vision_opencv.git
+```
+
+Then, we need to modify line 11 in `~/catkin_ws/src/vision_opencv/cv_bridge/CMakeLists.txt` from:
+
+`find_package(Boost REQUIRED python37)`
+
+to 
+
+`find_package(Boost REQUIRED python3)`
+
+Finally we build the package
+
+```shell
+cd ~/catkin_ws
+catkin_make
+```
--- src/ottobot_pkg/scripts/realsense.py
+++ src/ottobot_pkg/scripts/realsense.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-
-import pyrealsense2 as rs
-import numpy as np
-
 import rospy
 from std_msgs.msg import String, Int8
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+
+import pyrealsense2 as rs
+import numpy as np
+import cv2
 
 def config_camera(fps):
     # Create a pipeline
@@ -46,12 +46,12 @@ class RealSenseCameraNode(object):
                                         Image, 
                                         queue_size=10)
         
-        self.pipeline, config = config_camera(self.frame_rate)
+        self.pipeline, self.config = config_camera(self.frame_rate)
     
     def run(self):
 
         # Start streaming
-        profile = self.pipeline.start(config)
+        profile = self.pipeline.start(self.config)
         rospy.loginfo("Started streaming..")
 
         # Create an align object
@@ -65,44 +65,51 @@ class RealSenseCameraNode(object):
         try:
             while not rospy.is_shutdown():
                 # Get frameset of color and depth
-            frames = pipeline.wait_for_frames()
-            # frames.get_depth_frame() is a 640x360 depth image
+                frames = self.pipeline.wait_for_frames()
+                # frames.get_depth_frame() is a 640x360 depth image
 
-            # Get ROS time (this is NOT actually capturing time)
-            now = rospy.get_ros_time()
+                # Get ROS time (this is NOT actually capturing time)
+                now = rospy.get_rostime()
 
-            # Align the depth frame to color frame
-            aligned_frames = align.process(frames)
+                # Align the depth frame to color frame
+                aligned_frames = align.process(frames)
 
-            # Get aligned frames
-            aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
-            color_frame = aligned_frames.get_color_frame()
+                # Get aligned frames
+                aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+                color_frame = aligned_frames.get_color_frame()
 
-            # Validate that both frames are valid
-            if not aligned_depth_frame or not color_frame:
-                rospy.logwarn("Not valid frames, skipping them..")
-                continue
+                # Validate that both frames are valid
+                if not aligned_depth_frame or not color_frame:
+                    rospy.logwarn("Not valid frames, skipping them..")
+                    continue
 
-            depth_image = np.asanyarray(aligned_depth_frame.get_data())
-            color_image = np.asanyarray(color_frame.get_data())
+                depth_image = np.asanyarray(aligned_depth_frame.get_data())
+                color_image = np.asanyarray(color_frame.get_data())
 
-            # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-            depth_image = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+                # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+                depth_image = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-            # Convert images to ROS messages
-            try:
-                color_image_msg = bridge.cv2_to_imgmsg(color_image.astype(float), encoding="passthrough")
-                depth_image_msg = bridge.cv2_to_imgmsg(depth_image.astype(float), encoding="passthrough")
-            except CvBridgeError as e:
-                rospy.logerror(e)
-                continue
+                # Convert images to ROS messages
+                try:
+                    color_image_msg = bridge.cv2_to_imgmsg(color_image.astype(float), encoding="passthrough")
+                    depth_image_msg = bridge.cv2_to_imgmsg(depth_image.astype(float), encoding="passthrough")
+                except CvBridgeError as e:
+                    rospy.logerror(e)
+                    continue
 
-            color_image_msg.header.stamp = now
-            depth_image_msg.header.stamp = now
+                color_image_msg.header.stamp = now
+                depth_image_msg.header.stamp = now
 
-             # Publish images
-            self.rgb_pub.publish(color_image_msg)
-            self.depth_pub.publish(depth_image_msg)
+                # Debugging
+                # Show images
+                images = np.hstack((color_image, depth_image))
+                cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+                cv2.imshow('RealSense', images)
+                cv2.waitKey(1)
+
+                # Publish images
+                self.rgb_pub.publish(color_image_msg)
+                self.depth_pub.publish(depth_image_msg)
 
         finally:
             self.pipeline.stop()
